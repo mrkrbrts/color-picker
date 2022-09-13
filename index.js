8af4bbab73d125413ebae502d1cf9f868c6eb1c6
Mark Roberts <marker2911@gmail.com>
2022-09-13 17:52:15 +0100

    Added number of colors input

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const selectColor = document.getElementById("select-color")
 const selectMode = document.getElementById("select-mode")
+const selectNumber = document.getElementById("select-number")
 const getColorsBtn = document.getElementById("get-colors-button")
-
-numGridCols = 5;
 
 function renderGridCols(num) {
     document.getElementById("color-grid").innerHTML = ""
@@ -12,30 +11,32 @@ function renderGridCols(num) {
             <div>
                 <div class="grid-col color" id="color-${i}"></div>
                 <div class="color-info">
-                    <p class="hex" id="hex-${i}"></p>
+                    <button class="hex" id="hex-${i}"></button>
                 </div>
-            </div>`
+            </div>`    
     }
 }
 
 getColorsBtn.addEventListener("click", function() {
+    if (selectNumber.value > 0) {
+        renderGridCols(selectNumber.value)
     
-    //removes # from hex code so API can read it
-    let hex = selectColor.value.slice(1)
-
-    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${selectMode.value}&count=${numGridCols}`)
-    .then (res => res.json())
-    .then (data => {
-
-        for (let i=0; i < data.colors.length; i++) {
-
-            document.getElementById(`color-${i+1}`).style.backgroundColor = data.colors[i].hex.value;
-
-            document.getElementById(`hex-${i+1}`).innerHTML = data.colors[i].hex.value
-        }
-
-
-    })
+        //removes # from hex code so API can read it
+        let hex = selectColor.value.slice(1)
+    
+        fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${selectMode.value}&count=${selectNumber.value}`)
+        .then (res => res.json())
+        .then (data => {
+    
+            for (let i=0; i < data.colors.length; i++) {
+    
+                document.getElementById(`color-${i+1}`).style.backgroundColor = data.colors[i].hex.value;
+    
+                document.getElementById(`hex-${i+1}`).innerHTML = data.colors[i].hex.value
+            }
+            
+        })
+    } else {
+        alert("Please enter how many colors you want generated")
+    }
 })
-
-renderGridCols(numGridCols)
